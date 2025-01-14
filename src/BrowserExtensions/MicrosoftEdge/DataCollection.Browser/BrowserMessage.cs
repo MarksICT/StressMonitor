359b97335f2c358def8d32134d9ea0ef23672f9c
mjklaasen <mark.klaasen@gmail.com>
2025-01-14 23:26:01 +0100

    Added project for native host for Edge extension
    #7

---
+++ src/BrowserExtensions/MicrosoftEdge/DataCollection.Browser/BrowserMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace DataCollection.Browser;
+
+public class BrowserMessage
+{
+    public string Action { get; set; } = string.Empty;
+    public int TabId { get; set; }
+    public string Url { get; set; } = string.Empty;
+    public DateTime Time { get; set; }
+}
---
+++ src/BrowserExtensions/MicrosoftEdge/DataCollection.Browser/Program.cs
@@ -0,0 +1,8 @@
+﻿using DataCollection.Browser;
+
+// Continuously read messages
+while (true)
+{
+    var message = MessageProcessor.Read();
+    MessageProcessor.Write("Activity received");
+}
